f79f1e88407590b6c92069749d0067d3907d573d
Ed Silva <eed.silva@hotmail.com>
2018-07-02 13:45:32 -0700

    added latency column

--- app/src/components/TableProducers.js
+++ app/src/components/TableProducers.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Grid, Row, Col, Table } from 'react-bootstrap'
 import nodeInfoAPI from '../scripts/nodeInfo'
 import getHummanTime from '../scripts/timeHelper'
+import axios from 'axios';
 
 class TableProducers extends Component {
     constructor(props) {
@@ -15,13 +16,23 @@ class TableProducers extends Component {
             blocksProduced: [],
             blockTime: 0,
             lastTimeProduced: [],
-            lastIrrBlockNumber: 0
+            lastIrrBlockNumber: 0,
+            // currentLatency:0,
+            producersLatency: []
         }
     }
 
     async componentWillMount() {
         await this.getProducersInfo();
         await this.updateProducersInfo();
+    }
+
+    componentDidMount() {
+        let producerIndex = 0;
+        setInterval(() => {
+            this.getProducerLatency(producerIndex++);
+            if (producerIndex > this.state.producers.length - 1) producerIndex = 0;
+        }, 1000);
     }
 
     async getProducersInfo() {
@@ -35,6 +46,7 @@ class TableProducers extends Component {
     }
 
     async updateProducersInfo() {
+        let lastProducerIndex = 0;
         setInterval(async () => {
             let nodeInfo = await nodeInfoAPI.getInfo();
             let producerIndex = this.state.producers.findIndex((bp) => bp.owner == nodeInfo.head_block_producer);
@@ -47,6 +59,9 @@ class TableProducers extends Component {
 
                 lastTimeProduced = this.state.lastTimeProduced;
                 lastTimeProduced[producerIndex] = nodeInfo.head_block_time;
+
+                this.getProducerLatency(producerIndex);
+                lastProducerIndex = lastProducerIndex;
             }
 
             this.setState({
@@ -78,6 +93,33 @@ class TableProducers extends Component {
         return strProducerPercentage;
     }
 
+    getProducerLatency(producerIndex) {
+        let timeOut = 2000;
+        let timer;
+        let url = this.state.producers[producerIndex].url;
+        let pName = this.state.producers[producerIndex].owner;
+        let start = new Date().getTime();
+        let img = new Image();
+        img.src = url;
+        img.onload = pingCheck;
+        img.onerror = pingCheck;
+        let that = this;
+        timer = setTimeout(pingCheck(), timeOut);
+        function pingCheck(arg) {
+            if (timer) clearTimeout(timer);
+
+            let end = new Date().getTime();
+            let latency = end - start;
+
+            let pLatency = new Array(that.state.producers.length);
+            pLatency = that.state.producersLatency;
+            pLatency[producerIndex] = latency < timeOut ? latency : 0;
+            that.setState({
+                producersLatency: pLatency
+            });
+        }
+    }
+
     renderTableBody() {
         if (this.state.producers) {
             let body =
@@ -88,6 +130,7 @@ class TableProducers extends Component {
                                 <tr key={i}>
                                     <td>{val.owner == this.state.activeProducerName ? "Active" : i + 1}</td>
                                     <td>{val.owner}</td>
+                                    <td>{this.state.producersLatency[i]} ms</td>
                                     <td>{val.owner == this.state.activeProducerName ? this.state.currentBlockNumber : this.state.blocksProduced[i]} </td>
                                     <td>{val.owner == this.state.activeProducerName ?
                                         "producing..." :
@@ -110,12 +153,13 @@ class TableProducers extends Component {
                 <h4>Block version: {this.state.nodeVersion}</h4>
                 <h6>Block: {this.state.currentBlockNumber}</h6>
                 <h6>Last irreversible block: {this.state.lastIrrBlockNumber}</h6>
-                <div style={{ height: '30em', overflowY: 'scroll' }}>
+                <div style={{ height: '56em', overflowY: 'scroll' }}>
                     <Table responsive>
                         <thead>
                             <tr>
                                 <th>#</th>
                                 <th>Name</th>
+                                <th>Latency</th>
                                 <th>Last block</th>
                                 <th>Last time produced</th>
                                 {/* <th>Organization</th> */}
