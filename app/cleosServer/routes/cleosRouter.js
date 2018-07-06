c88af0d9fe7bb38a95c37d6e77e8686a4e3e06e9
Ed Silva <eed.silva@hotmail.com>
2018-07-06 11:20:40 -0700

    set cleos commands

--- app/cleosServer/routes/cleosRouter.js
+++ app/cleosServer/routes/cleosRouter.js
@@ -5,10 +5,28 @@ var express = require('express');
 var app = express();
 var cleosRouter = express.Router();
 
-cleosRouter.route('/').get((req, res) => {
+cleosRouter.route('/').post((req, res) => {
+    const SYMBOL = "EOS";
+    const MEMO = "Regiter producer account.";
+    const CPU = "1000";
+    const NET = "1000";
+    const RAM = "1000";
+    const AMOUNT = "1000";
+    var accountName = req.body.producer.name;
+    var ownerPubKey = req.body.producer.ownerPublicKey;
+    var activePubKey = req.body.producer.activePublicKey;
+    // console.log(req.body.producer);
+    const createAccountCMD = `cleos system newaccount --transfer eosio ${accountName} ${ownerPubKey} ${activePubKey} --stake-net "${NET}.0000 ${SYMBOL}" --stake-cpu "${CPU}.0000 ${SYMBOL}" --buy-ram "${RAM}.0000 ${SYMBOL}"`;
+    const transferCMD = `cleos transfer eosio ${accountName} "${AMOUNT}.0000 ${SYMBOL}" "${MEMO}"`;
     console.log("executing cleos...");
-    shell.exec('ls');
-
+    shell.echo("ECHO...");
+    shell.echo(createAccountCMD);
+    shell.echo(transferCMD);
+    shell.echo("END ECHO...");
+    shell.echo("EXEC...");
+    shell.exec(createAccountCMD);
+    shell.exec(transferCMD);
+    shell.echo("END EXEC...");
 });
 
 module.exports = cleosRouter;
