1e029f86e78ec8a3f297c0928f10a4db555afab5
Ed Silva <eed.silva@hotmail.com>
2018-07-10 15:12:08 -0700

    unclock wallet tecloserver

--- app/teclosServer/routes/teclosRouter.js
+++ app/teclosServer/routes/teclosRouter.js
@@ -20,17 +20,17 @@ teclosRouter.route('/').post((req, res) => {
     var activePubKey = req.body.producer.activePublicKey;
 
     // Running teclos commands
-    // const teclosUnlockWallet = '~/Documents/projects/telos/EOS-NODE/tlos-tool/tlos-tool.py --unlock';
+    const teclosUnlockWallet = '~/Documents/projects/telos/EOS-NODE/tlos-tool/tlos-tool.py --unlock';
     const createAccountCMD = `teclos system newaccount --transfer eosio ${accountName} ${ownerPubKey} ${activePubKey} --stake-net "${NET}.0000 ${SYMBOL}" --stake-cpu "${CPU}.0000 ${SYMBOL}" --buy-ram "${RAM}.0000 ${SYMBOL}"`;
     const transferCMD = `teclos transfer eosio ${accountName} "${AMOUNT}.0000 ${SYMBOL}" "${MEMO}"`;
     console.log("executing cleos...");
     shell.echo("ECHO...");
-    // shell.echo(teclosUnlockWallet);
+    shell.echo(teclosUnlockWallet);
     shell.echo(createAccountCMD);
     shell.echo(transferCMD);
     shell.echo("END ECHO...");
     shell.echo("EXEC...");
-    // shell.exec(teclosUnlockWallet);
+    shell.exec(teclosUnlockWallet);
     var createAccountResult = shell.exec(createAccountCMD);
     var transferResult = shell.exec(transferCMD);
     shell.echo("END EXEC...");
