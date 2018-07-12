7d4a4ad2645a32fd623b78fa67b9ae4c02c148d7
Ed Silva <eed.silva@hotmail.com>
2018-07-12 12:32:52 -0700

    init teclos info page

---
+++ app/src/components/TelosInfo.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class TelosInfo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <div>
+                <h3>[Telos info goes here]</h3>
+            </div>
+        );
+    }
+}
+
+export default TelosInfo;
