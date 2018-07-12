b70741185d77645cce325961ff7d42e8eadf6831
Ed Silva <eed.silva@hotmail.com>
2018-07-11 19:08:06 -0700

    displaying chain id on nodeifo component

--- app/src/components/NodeInfo.js
+++ app/src/components/NodeInfo.js
@@ -7,7 +7,8 @@ class NodeInfo extends Component {
         this.state = {
             nodeVersion: '-',
             currentBlockNumber: 0,
-            lastIrrBlockNumber: 0
+            lastIrrBlockNumber: 0,
+            chainId:""
         };
     }
 
@@ -23,17 +24,19 @@ class NodeInfo extends Component {
                 this.setState({
                     nodeVersion: nodeInfo.server_version,
                     currentBlockNumber: nodeInfo.head_block_num,
-                    lastIrrBlockNumber: nodeInfo.last_irreversible_block_num
+                    lastIrrBlockNumber: nodeInfo.last_irreversible_block_num,
+                    chainId: nodeInfo.chain_id
                 });
             }
         }, 1000);
     }
 
     render() {
-        const { nodeVersion, currentBlockNumber, lastIrrBlockNumber } = this.state;
+        const { nodeVersion, currentBlockNumber, lastIrrBlockNumber, chainId } = this.state;
         return (
             <div className='node_info'>
                 <h4>Node version: {nodeVersion}</h4>
+                <h6>Chain id: {chainId}</h6>
                 <h6>Block: {currentBlockNumber}</h6>
                 <h6>Last irreversible block: {lastIrrBlockNumber}</h6>
             </div>
