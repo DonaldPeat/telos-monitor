ddf2bf75b89aee442d44b78701131143f56a2934
Ed Silva <eed.silva@hotmail.com>
2018-07-12 11:49:12 -0700

    added api endpoint to netconfig file

--- app/src/config/netConfig.js
+++ app/src/config/netConfig.js
@@ -1,3 +1,4 @@
 export default {
-    endPoint: "http://localhost:8888/"
+    endPoint: "http://66.42.66.39:8888/",
+    apiEndpoint:"http://66.42.66.39:4200/"
 };
--- app/src/scripts/serverAPI.js
+++ app/src/scripts/serverAPI.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
-
+import netConfig from '../config/netConfig'
 export default {
     getEndpointLatency: async (url) => {
         try {
-            var result = await axios.post('http://localhost:4200/api/v1/latency', { host: url });
+            var result = await axios.post(netConfig.apiEndpoint + 'api/v1/latency', { host: url });
             return result.data;
         } catch (error) {
             console.error(error);
@@ -11,7 +11,7 @@ export default {
     },
 
     registerProducerNode: (producer, cb) => {
-        axios.post('http://localhost:4200/api/v1/producer', { producer: producer })
+        axios.post(netConfig.apiEndpoint + 'api/v1/producer', { producer: producer })
             .then((value) => {
                 cb(value);
             })
@@ -19,7 +19,7 @@ export default {
     },
 
     getAllAccounts: (cb) => {
-        axios.get('http://localhost:4200/api/v1/producer')
+        axios.get(netConfig.apiEndpoint + 'api/v1/producer')
             .then((value) => {
                 cb(value)
             })
