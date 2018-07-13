8d9cea7c99c4754c33156040145045f92d99bd15
Ed Silva <eed.silva@hotmail.com>
2018-07-13 12:22:04 -0700

    change net config endpoints

--- app/src/config/netConfig.js
+++ app/src/config/netConfig.js
@@ -1,5 +1,5 @@
 export default {
-    endPoint: "http://66.42.66.39:8888/",
-    apiEndpoint:"http://66.42.66.39:4200/",
-    teclosEndpoint:"http://66.42.66.39:5500/",
+    endPoint: "http://localhost:8888/",
+    apiEndpoint:"http://localhost:4200/",
+    teclosEndpoint:"http://localhost:5500/",
 };
