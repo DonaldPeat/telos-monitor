44d0739b26dc8028e26b9783ff866a5bb3f867ef
Justin A Brown <Justin.Brown345@gmail.com>
2018-08-06 15:43:42 -0700

    Redid producers update

--- app/src/components/Tables/TableProducers.js
+++ app/src/components/Tables/TableProducers.js
@@ -1,196 +1,167 @@
+import React, { Component } from 'react';
 import '../../styles/tableproducers.css'
-
-import React, {Component} from 'react';
-import {Alert, Col, ProgressBar, Row, Table} from 'react-bootstrap'
-
-import nodeInfoAPI from '../../scripts/nodeInfo'
-import serverAPI from '../../scripts/serverAPI';
-import getHumanTime from '../../scripts/timeHelper'
-import FormTextboxButton from '../FormControls/FormTextboxButton'
+import { Row, Col, Table, Alert } from 'react-bootstrap'
 import ModalProducerInfo from '../Modals/ModalProducerInfo'
+import nodeInfoAPI from '../../scripts/nodeInfo'
+import getHumanTime from '../../scripts/timeHelper'
+import serverAPI from '../../scripts/serverAPI';
 import ProducerMap from '../ProducerMap';
+import FormTextboxButton from '../FormControls/FormTextboxButton'
 
 class TableProducers extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      accounts: [],
-      producers: [],
-      activeProducerName: '',
-      //   totalVotesWheight: 0,
-      currentBlockNumber: 0,
-      blocksProduced: [],
-      blockTime: 0,
-      lastTimeProduced: [],
-      producersLatency: [],
-      showModalProducerInfo: false,
-      producerSelected: '',
-      producerFilter: '',
-      percentageVoteStaked: '',
-      totalVotesStaked: 0
-    }
-
-    this.totalTLOS = 190473249.0000;
-    this.updateProducersOrder = this.updateProducersOrder.bind(this);
-  }
-
-  componentWillMount() {
-    serverAPI.getAllAccounts(async(res) => {
-      this.setState({accounts: res.data});
-      if (await this.getProducersInfo()) {
-        await this.updateProducersInfo();
-      }
-    });
-  }
-
-  componentDidMount() {
-    let producerIndex = 0;
-    setInterval(async() => {
-      await this.getProducerLatency(producerIndex);
-      if (++producerIndex > this.state.producers.length - 1) producerIndex = 0;
-    }, 1000);
-
-    //update producers every 5 minutes
-    setInterval(this.updateProducersOrder, 300000);
-  }
-
-  //gets producers, reorders them
-  async updateProducersOrder(){
-    let newProd = [];
-    const {producers} = this.state;
-    const newProdData = await nodeInfoAPI.getProducers();
-    if(newProdData != null){
-      for(let i = 0; i < newProdData.rows.length; i++){
-        const thisOwner = newProdData.rows[i].owner;
-        const thisRow = producers.find(row => row.owner === thisOwner);
-        newProd[i] = thisRow;
-      }
-      //set state, remove empty values if they exist
-      this.setState({producers: newProd.filter(el => el.owner)});
-    }
-  }
-
-  async getProducersInfo() {
-    let data = await nodeInfoAPI.getProducers();
-    if (data != null) {
-      let producers = data.rows;
-      console.log(data);
-      this.setState({
-        producers: producers,
-        // totalVotesWheight: data.total_producer_vote_weight
-      });
-      return true;
-    } else
-      return false;
-    }
-
-  async updateProducersInfo() {
-    let data = await nodeInfoAPI.getGlobalState();
-    let totalVoteStaked = data.rows[0].total_activated_stake / 10000;
-
-    console.log('total Vote Staked: ', totalVoteStaked);
-
-    if (totalVoteStaked !== 0) {
-      let percentage = (totalVoteStaked * 100) / this.totalTLOS;
-      this.setState({
-        totalVotesStaked: totalVoteStaked,
-        percentageVoteStaked:
-            percentage.toString().match(/^-?\d+(?:\.\d{0,2})?/)[0]
-      });
-    }
-
-
-
-    if (this.state.producers.length > 0) {
-      setInterval(async() => {
-        let nodeInfo = await nodeInfoAPI.getInfo();
-        if (nodeInfo != null) {
-          let producerIndex = this.state.producers.findIndex(
-              (bp) => bp.owner === nodeInfo.head_block_producer);
-          let blocksProduced = new Array(this.state.producers.length);
-          let lastTimeProduced = new Array(this.state.producers.length);
-
-          if (producerIndex > -1) {
-            blocksProduced = this.state.blocksProduced;
-            blocksProduced[producerIndex] = nodeInfo.head_block_num;
-
-            lastTimeProduced = this.state.lastTimeProduced;
-            lastTimeProduced[producerIndex] = nodeInfo.head_block_time;
-
-            await this.getProducerLatency(producerIndex);
-          }
-
-          this.setState({
-            activeProducerName: nodeInfo.head_block_producer,
-            currentBlockNumber: nodeInfo.head_block_num,
-            blockTime: nodeInfo.head_block_time,
-            blocksProduced: blocksProduced
-          });
+    constructor(props) {
+        super(props);
+        this.state = {
+            accounts: [],
+            producers: [],
+            activeProducerName: '',
+            totalVotesWheight: 0,
+            currentBlockNumber: 0,
+            blocksProduced: [],
+            blockTime: 0,
+            lastTimeProduced: [],
+            producersLatency: [],
+            showModalProducerInfo: false,
+            producerSelected: '',
+            producerFilter: ''
         }
-      }, 1000);
     }
-  }
 
-  getLastTimeBlockProduced(bpLastTimeProduced, headBlockTime) {
-    let bpTime = new Date(bpLastTimeProduced);
-    let headTime = new Date(headBlockTime);
-    bpTime = Number(bpTime);
-    headTime = Number(headTime);
-
-    let lastTimeProduced = getHumanTime(Math.floor(headTime - bpTime) / 1000);
-    return lastTimeProduced;
-  }
-
-  getProducerPercentage(bp) {
-    if (parseFloat(bp.total_votes) > 0 && this.state.totalVotesStaked > 0) {
-      let producerPercentage = (parseFloat(bp.total_votes / 100)) / parseFloat(this.state.totalVotesStaked);
-      let strProducerPercentage = producerPercentage.toString().match(/^-?\d+(?:\.\d{0,2})?/)[0];
-
-       return strProducerPercentage;
-      }
-    else{
-      return 0;
+    componentWillMount() {
+        serverAPI.getAllAccounts(async (res) => {
+            this.setState({
+                accounts: res.data
+            });
+            if (await this.getProducersInfo()) {
+                await this.updateProducersInfo();
+            }
+        });
     }
-  }
-  async getProducerLatency(producerIndex) {
-    if (this.state.producers.length > 0 && this.state.accounts.length > 0) {
-      let producerName = this.state.producers[producerIndex].owner;
-      let matchedProducer =
-          this.state.accounts.find((item) => item.name === producerName);
-      if (matchedProducer) {
-        let url = matchedProducer.p2pServerAddress;
-        let result = await serverAPI.getEndpointLatency(url);
-        let pLatency = new Array(this.state.producers.length);
-        pLatency = this.state.producersLatency;
-        let latency = result != null ? result.latency : '-';
-        pLatency[producerIndex] =
-            latency != '-' ? latency < 1000 ? latency : '-' : '-';
-        this.setState({producersLatency: pLatency});
-      }
+
+    componentDidMount() {
+        let producerIndex = 0;
+        setInterval(async () => {
+            await this.getProducerLatency(producerIndex);
+            if (++producerIndex > this.state.producers.length - 1) producerIndex = 0;
+        }, 1000);
+
+        //update producers every 5 minutes
+        setInterval(this.updateProducersOrder, 300000);       
     }
-  }
 
-  showProducerInfo(producerSelected) {
-    this.setState({
-      producerSelected: producerSelected,
-    });
-    this.forceUpdate(() => {
-      this.setState({showModalProducerInfo: !this.state.showModalProducerInfo});
-    });
-  }
+    //gets producers, reorders them
+    async updateProducersOrder(){
+        let newProd = [];
+        const {producers} = this.state;
+        const newProdData = await nodeInfoAPI.getProducers();
+        if(newProdData != null){
+          for(let i = 0; i < newProdData.rows.length; i++){
+            const thisOwner = newProdData.rows[i].owner;
+            const thisRow = producers.find(row => row.owner === thisOwner);
+            newProd[i] = thisRow;
+          }
+          //set state, remove empty values if they exist
+          this.setState({producers: newProd.filter(el => el.owner)});
+        }
+    }    
 
-  renderTableBody() {
-    if (this.state.producers.length > 0) {
-      let prods;
+    async getProducersInfo() {
+        let data = await nodeInfoAPI.getProducers();
+        if (data != null) {
+            let producers = data.rows;
+            this.setState({
+                producers: producers,
+                totalVotesWheight: data.total_producer_vote_weight
+            });
+            return true;
+        } else return false;
+    }
 
-      if (this.state.producerFilter === '')
-        prods = this.state.producers.filter(val => val.is_active === 1);
-      else
-        prods = this.state.producers.filter(
-            val => val.is_active === 1 &&
-                val.owner.includes(this.state.producerFilter));
+    async updateProducersInfo() {
+        if (this.state.producers.length > 0) {
+            setInterval(async () => {
+                let nodeInfo = await nodeInfoAPI.getInfo();
+                if (nodeInfo != null) {
+                    let producerIndex = this.state.producers.findIndex((bp) => bp.owner === nodeInfo.head_block_producer);
+                    let blocksProduced = new Array(this.state.producers.length);
+                    let lastTimeProduced = new Array(this.state.producers.length);
 
-      let body = <tbody> {
+                    if (producerIndex > -1) {
+                        blocksProduced = this.state.blocksProduced;
+                        blocksProduced[producerIndex] = nodeInfo.head_block_num;
+
+                        lastTimeProduced = this.state.lastTimeProduced;
+                        lastTimeProduced[producerIndex] = nodeInfo.head_block_time;
+
+                        await this.getProducerLatency(producerIndex);
+                    }
+
+                    this.setState({
+                        activeProducerName: nodeInfo.head_block_producer,
+                        currentBlockNumber: nodeInfo.head_block_num,
+                        blockTime: nodeInfo.head_block_time,
+                        blocksProduced: blocksProduced
+                    });
+                }
+            }, 1000);
+        }
+    }
+
+    getLastTimeBlockProduced(bpLastTimeProduced, headBlockTime) {
+        let bpTime = new Date(bpLastTimeProduced);
+        let headTime = new Date(headBlockTime);
+        bpTime = Number(bpTime);
+        headTime = Number(headTime);
+
+        let lastTimeProduced = getHumanTime(Math.floor(headTime - bpTime) / 1000);
+        return lastTimeProduced;
+    }
+
+    getProducerPercentage(bp) {
+        if (parseFloat(bp.total_votes) > 0) {
+            let producerPercentage = (parseFloat(bp.total_votes * 100)) / parseFloat(this.state.totalVotesWheight);
+            let strProducerPercentage = producerPercentage.toString().match(/^-?\d+(?:\.\d{0,2})?/)[0];
+
+            return strProducerPercentage;
+        }
+        else return 0;
+    }
+
+    async getProducerLatency(producerIndex) {
+        if (this.state.producers.length > 0 && this.state.accounts.length > 0) {
+            let producerName = this.state.producers[producerIndex].owner;
+            let matchedProducer = this.state.accounts.find((item) => item.name === producerName);
+            if(matchedProducer){
+                let url = matchedProducer.p2pServerAddress;
+                let result = await serverAPI.getEndpointLatency(url);
+                let pLatency = new Array(this.state.producers.length);
+                pLatency = this.state.producersLatency;
+                let latency = result != null ? result.latency : "-";
+                pLatency[producerIndex] = latency != "-" ? latency < 1000 ? latency : "-" : "-";
+                this.setState({
+                    producersLatency: pLatency
+                });    
+            }
+        }
+    }
+
+    showProducerInfo(producerSelected) {
+        this.setState({
+            producerSelected: producerSelected,
+            showModalProducerInfo: !this.state.showModalProducerInfo
+        });
+    }
+
+    renderTableBody() {
+        if (this.state.producers.length > 0) {
+            let prods; 
+            
+          if(this.state.producerFilter==="") prods = this.state.producers.filter(val => val.is_active === 1);
+          else prods = this.state.producers.filter(val => val.is_active === 1 && val.owner.includes(this.state.producerFilter));
+            
+          let body =
+                <tbody>
+                    {
                         prods.map((val, i) => {
                             return (
                                 <tr key={i} className={val.owner === this.state.activeProducerName ? 'activeProducer' : ''}>
@@ -247,13 +218,6 @@ class TableProducers extends Component {
                                 onChange={(arg) => this.onProducerFilterChange(arg)}
                                 placeHolder="Filter by producer name"
                             />
-                        </Col>
-                        <Col sm={2}>
-                            <p>Voting progress</p>
-                        </Col>
-                        <Col sm={10}>
-                            <ProgressBar active now={this.state.percentageVoteStaked} bsStyle="success" 
-                                label={this.state.percentageVoteStaked == "" ? "": `${this.state.percentageVoteStaked}%`} />
                         </Col>
                     </Row>
                     <Row>
