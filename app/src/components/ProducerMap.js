37c3ff4f9a153303378033b354ead57746632ae3
Justin A Brown <Justin.Brown345@gmail.com>
2018-08-07 16:58:27 -0700

    added info icon in map InfoWindow

--- app/src/components/ProducerMap.js
+++ app/src/components/ProducerMap.js
@@ -51,6 +51,7 @@ const ProducerMap = ({ip_locations, producers}) => {
 	});
 	return (
 		<GoogleMap
+			onClick={e => console.log(e)}
 			defaultZoom={2}
 			defaultCenter={{lat: 13.491665, lng: -92.508646}}
 			defaultOptions={{
@@ -72,7 +73,8 @@ class MarkerWithInfo extends Component {
 	constructor(props){
 		super(props);
 		this.state = {
-			open: false
+			open: false,
+			infoBoxOpen: false
 		};
 	}
 	render(){
@@ -87,28 +89,44 @@ class MarkerWithInfo extends Component {
  				{this.state.open && 
 				<InfoWindow position={{lat: this.props.loc.latitude, lng: this.props.loc.longitude}}
 			                onCloseClick={() => this.setState({open: false})}>
-				    <div>
+				    <div style={{position: 'relative'}}>
 				    	<h4>{producer.name}</h4>
 				    	<h5>{producer.organization}</h5>
 				    	<table className='map_info_table'>
-				    		<tr>
-				    			<td>Server Address:</td>
-				    			<td> {producer.httpServerAddress ? producer.httpServerAddress : producer.httpsServerAddress}</td>
-				    		</tr>
-				    		{producer.url ? 
-				    			<tr>
-					    			<td>URL:</td>
-					    			<td> {producer.url}</td>
-					    		</tr>
-				    			: ''
-				    		}
-				    		{producer.p2pServerAddress ?
+				    		<tbody>
 					    		<tr>
-					    			<td>Peer Server Address:</td>
-					    			<td>{producer.p2pServerAddress ? producer.p2pServerAddress : ''}</td>
-					    		</tr>	
-				    			: ''
-				    		}
+					    			<td>Server Address:</td>
+					    			<td> 
+					    				{producer.httpServerAddress ? producer.httpServerAddress : producer.httpsServerAddress}&nbsp; 
+					    				<i 
+					    					className='fa fa-info-circle' 
+					    					onClick={(e) => {
+					    						const iconRect = e.target.getBoundingClientRect();
+					    						this.setState({infoBoxOpen: !this.state.infoBoxOpen});
+					    					}}
+					    				></i>
+										{this.state.infoBoxOpen &&
+											<span> 
+												<br /><p>Location info from block producer registration</p>
+											</span>
+										}
+					    			</td>
+					    		</tr>
+					    		{producer.url ? 
+					    			<tr>
+						    			<td>URL:</td>
+						    			<td> {producer.url}</td>
+						    		</tr>
+					    			: ''
+					    		}
+					    		{producer.p2pServerAddress ?
+						    		<tr>
+						    			<td>Peer Server Address:</td>
+						    			<td>{producer.p2pServerAddress ? producer.p2pServerAddress : ''}</td>
+						    		</tr>	
+					    			: ''
+					    		}
+				    		</tbody>
 				    	</table>
  				    </div>
 			    </InfoWindow>}
