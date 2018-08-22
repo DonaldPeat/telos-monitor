0f4e1174ae1d7b6aebeeb252cb478f7f132f95e4
Ed Silva <eed.silva@hotmail.com>
2018-08-22 16:09:39 -0700

    added ui validation to the form

--- app/src/components/Navigation.js
+++ app/src/components/Navigation.js
@@ -43,9 +43,8 @@ class Navigation extends Component {
 							<NavItem onClick={() => this.props.history.push('/blocks')}>BLOCKS</NavItem>
 							<NavItem onClick={() => this.props.history.push('/transactions')}>TRANSACTIONS</NavItem>
 							<NavItem onClick={() => this.props.history.push('/p2plist')}>P2P LIST</NavItem>
-							{/* <NavItem onClick={() => this.props.history.push('/stages/stage-1')}>STAGES</NavItem> */}
 							<NavItem onClick={() => this.showModalRegisterProducer()}>REGISTER</NavItem>
-							{/* <NavItem onClick={() => this.showModalCreateAccount()} >CREATE ACCOUNT</NavItem> */}
+							<NavItem onClick={() => this.showModalCreateAccount()} >CREATE ACCOUNT</NavItem>
 						</Nav>
 					</Navbar.Collapse>
 				</Navbar>
